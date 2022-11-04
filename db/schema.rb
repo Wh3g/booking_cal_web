8519ee3f24f3790e5fd6a61bd59b580436f3f58a
Willoughby Hood <willoughbyhood29@googlemail.com>
2022-11-04 14:56:40 +0000

    users have admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_034340) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_145338) do
   create_table "bookings", force: :cascade do |t|
     t.string "name"
     t.string "service"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_034340) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,14 @@ RSpec.describe User, type: :model do
     it "is not valid without a password" do
       expect(User.new(email: email, password: nil)).to_not be_valid
     end
+
+    it "is valid with #admin boolean" do
+      expect(User.new(email: email, password: password, admin: true)).to be_valid
+    end
+
+    it "admin defaults to false" do
+      user = User.new(email: email, password: password)
+      expect(user.admin).to eq false
+    end
   end
 end
